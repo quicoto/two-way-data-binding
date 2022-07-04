50f7b18087f09242bf97facaf1963498ef91a4bf
Ricard Torres <torres.rick@gmail.com>
2022-07-04 17:04:07 +0200

    feat: adds Jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  testEnvironment: `jsdom`,
+  transform: {
+    "^.+\\.jsx?$": `babel-jest`
+  },
+  setupFilesAfterEnv: [`<rootDir>/jest-setup.js`]
+};
+
+module.exports = config;
